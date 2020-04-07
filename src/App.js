32368de87a1ba09e847e260132cfc9da15c0b266
Darcy Price <darcyelsing@gmail.com>
2020-04-07 16:14:56 +1000

    included google api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import styled from 'styled-components';
 import Map from './components/Map';
 
 export default function App() {
+  const url =
+    'https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=AIzaSyAUyTbmTb_mwCdi_76hfeoPF-vqiKRS4gM';
+
+  const getDirections = () => {
+    fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Conrol-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'GEST, POST, PUT, DELETE, OPTIONS',
+      },
+    }).then((resp) => {
+      console.log(resp);
+    });
+  };
+
+  getDirections();
+
   return (
     <StyledMain>
       <Map />
